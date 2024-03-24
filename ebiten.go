85ded295db13b120b098712fe390759517ef5778
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2024-03-24 19:37:54 +0200

    Added drawing to console and clearing screen

--- ebiten.go
+++ ebiten.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 const (
 	screenWidth, screenHeight = 500, 500
-	rows, cols                = 28, 28
+	rowCount, colCount        = 28, 28
 	title                     = "mnist"
-	strokeWidth               = 1
+	strokeWidth               = 2
 )
 
 type App struct {
@@ -27,14 +29,59 @@ func NewGame() *App {
 	}
 }
 
-func (a *App) Layout(outWidth, outHeight int) (w, h int) { return cols, rows }
+func (a *App) Layout(outWidth, outHeight int) (w, h int) { return colCount, rowCount }
 func (a *App) Update() error {
-	a.handleDrawing()
+	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
+		a.drawScreenInConsole()
+	} else if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		a.handleDrawing()
+	} else if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
+		a.screenBuffer.Clear()
+		a.resetMousePrevCoord()
+	}
 	a.updateMousePrevCoord()
 	return nil
 }
+
 func (a *App) Draw(screen *ebiten.Image) {
 	screen.DrawImage(a.screenBuffer, &ebiten.DrawImageOptions{})
+}
+
+// Colours pixels under the cursor with shades of gray when LMB is pressed
+// using cursor position from the previous frame(pass -1, -1 if it's the first frame).
+func (a *App) handleDrawing() {
+	x, y := ebiten.CursorPosition()
+	if a.mousePrevX == -1 && a.mousePrevY == -1 {
+		a.screenBuffer.Set(x, y, color.White)
+	} else {
+		vector.StrokeLine(a.screenBuffer, float32(a.mousePrevX), float32(a.mousePrevY), float32(x), float32(y), strokeWidth, color.White, true)
+	}
+}
+
+func (a *App) updateMousePrevCoord() {
+	a.mousePrevX, a.mousePrevY = ebiten.CursorPosition()
+}
+
+func (a *App) resetMousePrevCoord() {
+	a.mousePrevX, a.mousePrevY = -1, -1
+}
+
+// Draws screen buffer's content with ASCII characters in console.
+func (a *App) drawScreenInConsole() {
+	for r := 0; r < rowCount; r++ {
+		for c := 0; c < colCount; c++ {
+			// Type assertions - https://go.dev/tour/methods/15
+			alpha := a.screenBuffer.At(c, r).(color.RGBA).A
+			if alpha == 0 {
+				fmt.Print(" ")
+			} else if alpha < 128 { // 255/2 = 127.5 ~ 128
+				fmt.Print(".")
+			} else {
+				fmt.Print("#")
+			}
+		}
+		fmt.Println()
+	}
 }
 
 // Initialises app and runs main loop that handles drawing on screen.
@@ -44,21 +91,4 @@ func RunDrawing() {
 	if err := ebiten.RunGame(NewGame()); err != nil {
 		log.Fatal(err)
 	}
-}
-
-// Colours pixels under the cursor with shades of gray when LMB is pressed
-// using cursor position from the previous frame(pass -1, -1 if it's the first frame).
-func (a *App) handleDrawing() {
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-		x, y := ebiten.CursorPosition()
-		if a.mousePrevX == -1 && a.mousePrevY == -1 {
-			a.screenBuffer.Set(x, y, color.White)
-		} else {
-			vector.StrokeLine(a.screenBuffer, float32(a.mousePrevX), float32(a.mousePrevY), float32(x), float32(y), strokeWidth, color.White, true)
-		}
-	}
-}
-
-func (a *App) updateMousePrevCoord() {
-	a.mousePrevX, a.mousePrevY = ebiten.CursorPosition()
 }
